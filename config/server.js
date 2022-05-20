8ad02b60b293b67e00a3bf685480553e9980326a
nabilputrah <nabilputra28.ns@gmail.com>
2022-05-20 14:16:08 +0700

    t

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f1eedc8ded0300c09640ab9480a155bb'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '52707a79-a995-4033-8be0-00290c105084'
-};
