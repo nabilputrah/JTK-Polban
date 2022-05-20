0617ed6f20ef32ff403de7de6c3751744998a4c6
andikayudh182 <61397720+andikayudh182@users.noreply.github.com>
2022-05-20 14:41:23 +0700

    inisiate ceemes
    inisiate ceemes

---
+++ cms-jtkpolban/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96bd61a2c8763d4ce0287a194dd2751e'),
+    },
+  },
+});
---
+++ cms-jtkpolban/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0aaa766a-e2b0-463e-b57c-7f2d455e32c5'
+};
