2fdcedcc5c01172a4a6486de1dff1195872496d8
andikayudh182 <61397720+andikayudh182@users.noreply.github.com>
2022-05-28 17:54:23 +0700

    nambah data

--- cms-jtkpolban/api/arsip-kerjasama/controllers/arsip-kerjasama.js
+++ cms-jtkpolban/api/arsip-kerjasama/controllers/arsip-kerjasama.js
@@ -1,8 +1,21 @@
-'use strict';
+// const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  // /**
+  //  * Retrieve a record.
+  //  *
+  //  * @return {Object}
+  //  */
 
-module.exports = {};
+  // async findOne(ctx) {
+  //   const { slug } = ctx.params;
+
+  //   const entity = await strapi.services.arsip-kerjasama.findOne({ slug });
+  //   return sanitizeEntity(entity, { model: strapi.models.arsip-kerjasama });
+  // },
+  // // API Khusus Buat News
+  // async index(){
+  //   const response = await strapi.services.article.find({ _limit: 4, _sort: 'published_at:desc'});
+  //   return sanitizeEntity(response, { model: strapi.models.article });
+  // },
+};
