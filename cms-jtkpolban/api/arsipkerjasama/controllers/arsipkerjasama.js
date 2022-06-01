56dcd326252ba1f215b5821e62e2d5f97e4c20d9
andikayudh182 <61397720+andikayudh182@users.noreply.github.com>
2022-06-01 09:59:12 +0700

    push cms
    push cms

--- cms-jtkpolban/api/arsipkerjasama/controllers/arsipkerjasama.js
+++ cms-jtkpolban/api/arsipkerjasama/controllers/arsipkerjasama.js
@@ -7,8 +7,13 @@ module.exports = {
    * @return {Object}
    */
 
- 
-  // API Khusus Buat Arsip Kerjasama
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.arsipkerjasama.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.arsipkerjasama });
+  },
+  // API Khusus Buat index
   async index(){
     const response = await strapi.services.arsipkerjasama.find({ _limit: 4, _sort: 'published_at:desc'});
     return sanitizeEntity(response, { model: strapi.models.arsipkerjasama });
--- cms-jtkpolban/api/prestasi/controllers/prestasi.js
+++ cms-jtkpolban/api/prestasi/controllers/prestasi.js
@@ -1,8 +1,31 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.prestasi.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.prestasi });
+  },
+
+  async indexPenghargaan(){
+    const response = await strapi.query('prestasi').find({'category.name':'Penghargaan', _limit:4, _sort: 'published_at:desc'});
+    return sanitizeEntity(response, { model: strapi.models.prestasi });
+  },
+
+  async indexPKM(){
+    const response = await strapi.query('prestasi').find({'category.name':'Program Kreativitas Mahasiswa', _limit:4, _sort: 'published_at:desc'});
+    return sanitizeEntity(response, { model: strapi.models.prestasi });
+  },
+
+  async indexMapres(){
+    const response = await strapi.query('prestasi').find({'category.name':'Mahasiswa Berprestasi', _limit:4, _sort: 'published_at:desc'});
+    return sanitizeEntity(response, { model: strapi.models.prestasi });
+  },
+};
